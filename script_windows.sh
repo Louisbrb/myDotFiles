10ca2221aaab262da07f8e1c2d8ce597b696e133
louisbrb <louisburban@yahoo.fr>
2025-05-19 17:35:46 +0200

    another autocommit

--- script_windows.sh
+++ script_windows.sh
@@ -3,8 +3,8 @@ eval "$(ssh-agent -s)"
 ssh-add /home/lbn/sshKeys/sshKeyGithub
 
 git pull
-cp /home/lbn/.config/nvim/* neovim/nvim/
-cp /mnt/c/Users/LBN/.config/wezterm/* wezterm
+cp -r /home/lbn/.config/nvim/* neovim/nvim/
+cp -r /mnt/c/Users/LBN/.config/wezterm/* wezterm
 
 git add -A
 git commit -m "another autocommit"
